0a6ac7d8c715eb96929506e1838a2d128a1a8802
Azad Asanali <azneotech@gmail.com>
2023-01-23 23:22:55 +0800

    day 1 theory & exercise - completed

---
+++ day-1-printing/exercise/main.py
@@ -0,0 +1,57 @@
+# Printing to the console
+print("Hello World!")
+
+# Exercise - 1 Printing
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# String manipulation and code intelligence
+print("Hello" + " " + "Azad")
+
+# Exercise - 2 Debugging practice
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+# Input function
+print("Hello " + input("What is your name?") + "!")
+
+# Exercise - 3 Inputs assignment
+print(len(input("What is your name? ")))
+
+# Variables
+
+name = "Jack"
+print(name)
+
+name = "Angela"
+print(name)
+
+name = input("What is your name?")
+length = len(name)
+print(length)
+
+# Exercise - 4 Variables
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+tmp = a
+a = b
+b = tmp
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
