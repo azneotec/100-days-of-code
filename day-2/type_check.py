c5bfc759be8677159602010766ef6fe44acb0cc1
Azad Asanali <azneotech@gmail.com>
2023-01-24 11:27:52 +0800

    day 2: type check, conversion

---
+++ day-2/type_check.py
@@ -0,0 +1,18 @@
+num_char = len(input("What is your name?"))
+# print("Your name has " + num_char + " characters.")
+
+# Type checking
+print(type(num_char))
+
+# Type conversion
+new_num_char = str(num_char)
+print("Your name has " + new_num_char + " characters.")
+
+a = str(123)
+print(type(a))
+
+b = float(a)
+print(type(b))
+
+print(70 + float("100.5"))
+print(str(70) + str(100))
