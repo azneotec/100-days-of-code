749699cb987da3d89ebcb454b2c76e74ce8faebf
Azad Asanali <azneotech@gmail.com>
2023-01-24 11:21:35 +0800

    day 2: data types

---
+++ day-2/data_types.py
@@ -0,0 +1,24 @@
+# Data Types
+
+# String
+
+print("Hello"[0])
+
+"Hello"[4]
+
+print("123" + "345")
+
+# Integer
+
+print(123 + 345)
+
+print(123_456_789)
+
+# Float
+
+3.14159
+
+# Boolean
+
+True
+False
