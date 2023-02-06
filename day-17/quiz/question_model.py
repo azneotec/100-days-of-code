15c5692d96c01612d364b88aa144017a7907884a
Azad Asanali <azneotech@gmail.com>
2023-02-06 21:32:03 +0800

    day 17: Quiz project part 2 - creating the lisr of question objects from the data

--- day-17/quiz/question_model.py
+++ day-17/quiz/question_model.py
@@ -1,6 +1,10 @@
 class Question:
 
-    def __init__(self, text, answer):
-        self.text = text
-        self.answer = answer
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+
+    def __str__(self):
+        attrs = vars(self)
+        return 'Question({})'.format(', '.join('{}={}'.format(k, v) for k, v in attrs.items()))
 
