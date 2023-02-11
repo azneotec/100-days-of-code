f00ddd0d00fe19b1545fb2b10ae12d892c769c13
Azad Asanali <azneotech@gmail.com>
2023-02-11 09:32:03 +0800

    day 21: Snake game - Detect collision with wall

--- day-20-21/snake-game/scoreboard.py
+++ day-20-21/snake-game/scoreboard.py
@@ -21,4 +21,7 @@ class Scoreboard(Turtle):
     def increase_score(self):
         self.score += 1
         self.update_scoreboard()
-        pass
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
