baba1329328286af694c67b74b5434087d79c496
Azad Asanali <azneotech@gmail.com>
2023-02-12 21:46:36 +0800

    day 22: Score keeping and changing the ball speed

--- day-22/pong-game/main.py
+++ day-22/pong-game/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -12,7 +13,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
-
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -22,7 +23,7 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -30,7 +31,7 @@ while game_is_on:
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
 
-    # Detect collision with r_paddle
+    # Detect collision with paddle
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 \
             or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
@@ -38,9 +39,11 @@ while game_is_on:
     # Detect R paddle misses
     if ball.xcor() > 380:
         ball.reset_position()
+        scoreboard.l_point()
 
     # Detect L paddle misses
     if ball.xcor() < -380:
         ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
