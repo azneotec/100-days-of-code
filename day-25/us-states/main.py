06f0022bea646a75a156ec3b471e0d9619389d75
Azad Asanali <azneotech@gmail.com>
2023-02-18 16:56:48 +0800

    day 25: Project - U.S. States Game Part_2

--- day-25/us-states/main.py
+++ day-25/us-states/main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -6,7 +7,25 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state name?")
-print(answer_state)
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(
+        title=f"{len(guessed_states)}/50 States Correct",
+        prompt="What's another state name?").title()
+
+    # If answer_state is one of the states in all the states of the 50_states.csv
+    #   If they got it right:
+    #       Create a turtle to write the name of the state at the state's x and y cooridanates
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
 
 screen.exitonclick()
