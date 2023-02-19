22f8a6fecef41415876fb036ffb1c7d5173eae03
Azad Asanali <azneotech@gmail.com>
2023-02-19 13:10:00 +0800

    day 26: Create list using list comprehension

---
+++ day-26/th_1_list_comp.py
@@ -0,0 +1,7 @@
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+short_names = [name for name in names if len(name) < 5]
+
+print(short_names)
+
+long_names = [name.upper() for name in names if len(name) > 5]
+print(long_names)
