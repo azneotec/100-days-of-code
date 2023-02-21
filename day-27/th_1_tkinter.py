280e0d83c5c24bee74b3694301781367f678b4f5
Azad Asanali <azneotech@gmail.com>
2023-02-21 21:17:50 +0800

    day 27: Tkinter layout managers

--- day-27/th_1_tkinter.py
+++ day-27/th_1_tkinter.py
@@ -3,10 +3,14 @@ from tkinter import *
 window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
 
 # Label
 my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
-my_label.pack()
+# my_label.pack(side="left")
+# my_label.place(x=100, y=200)
+my_label.grid(row=0, column=0)
+my_label.config(padx=50, pady=50)
 
 my_label["text"] = "New Text"
 # my_label.config(text="New Text")
@@ -22,11 +26,16 @@ def button_clicked():
 
 
 button = Button(text="Click Me", command=button_clicked)
-button.pack()
+# button.pack(side="left")
+button.grid(row=1, column=1)
+
+new_button = Button(text="New Button")
+new_button.grid(row=0, column=2)
 
 # Entry
 input = Entry(width=10)
-input.pack()
+# input.pack(side="left")
 # print(input.get())
+input.grid(row=2, column=3)
 
 window.mainloop()
