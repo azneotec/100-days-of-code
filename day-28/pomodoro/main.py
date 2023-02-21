e5631a6c74552a25ba67540bcaa76a1e24cf8bf7
Azad Asanali <azneotech@gmail.com>
2023-02-21 22:02:20 +0800

    day 28: Project: Pomodoro Application - Completed User Interface

--- day-28/pomodoro/main.py
+++ day-28/pomodoro/main.py
@@ -21,10 +21,22 @@ window = Tk()
 window.title("Pomodoro Application")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+timer_label.grid(row=0, column=1)
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(row=1, column=1)
+
+start_button = Button(text="Start", highlightbackground=YELLOW)
+start_button.grid(row=2, column=0)
+
+reset_button = Button(text="Reset", highlightbackground=YELLOW)
+reset_button.grid(row=2, column=2)
+
+check_marks = Label(text="✔", fg=GREEN, bg=YELLOW)
+check_marks.grid(row=3, column=1)
 
 window.mainloop()
