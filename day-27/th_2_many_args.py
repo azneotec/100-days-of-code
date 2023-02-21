7b20e2f5d1efa8c68db8211a1acf254b8718023b
Azad Asanali <azneotech@gmail.com>
2023-02-21 20:40:53 +0800

    day 27: Many keyword arguments

--- day-27/th_2_many_args.py
+++ day-27/th_2_many_args.py
@@ -1,5 +1,5 @@
 def add(*args):
-    # It's a tuple
+    # *args -> It's a tuple
     print(args[0])
     sum = 0
     for n in args:
@@ -8,3 +8,30 @@ def add(*args):
 
 
 print(add(1, 2, 3, 4, 5))
+
+
+def calculate(n, **kwargs):
+    # **kwargs -> It's a dictionary
+    print(kwargs)
+
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+
+calculate(2, add=3, multiply=5)
+
+
+class Car:
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+
+
+my_car = Car(make="Nissan", model="GT-R")
+print(my_car.make)
+print(my_car.model)
+
+my_car_2 = Car(make="Tesla")
+print(my_car_2.make)
+print(my_car_2.model)
