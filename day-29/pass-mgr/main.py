b639ddc93eda9c592bd8cf8a214a38bf8e60a01c
Azad Asanali <azneotech@gmail.com>
2023-02-23 21:42:06 +0800

    day 29: Project: Password Manager Application - Saving data to file

--- day-29/pass-mgr/main.py
+++ day-29/pass-mgr/main.py
@@ -1,7 +1,21 @@
+import os
 from tkinter import *
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website} | {email} | {password}\n")
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
+    pass
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -22,14 +36,16 @@ password_label.grid(row=3, column=0)
 
 website_entry = Entry(width=38)
 website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
 email_entry = Entry(width=38)
 email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(END, "abcd1234@fakegmail.com")
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
