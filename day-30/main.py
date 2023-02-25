65af6126a8bd024c32d88030fe6eb29347dcdb64
Azad Asanali <azneotech@gmail.com>
2023-02-25 09:29:41 +0800

    day 30: raise own exceptions

--- day-30/main.py
+++ day-30/main.py
@@ -29,3 +29,13 @@ else:
 finally:
     file.close()
     print("File was closed.")
+    raise TypeError("This is an error that I made up.")
+
+height = float(input("Height: "))
+weight = int(input("Weight: "))
+
+if height > 3:
+    raise ValueError("Human Height should not be over 3 meters.")
+
+bmi = weight / height ** 2
+print(bmi)
