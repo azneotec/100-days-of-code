266f5535478ec6b43e0d3c35fc735e7e2112df4a
Azad Asanali <azneotech@gmail.com>
2023-03-04 15:08:09 +0800

    day 32: Challenge 1 - Send motivational quotes on monday

--- day-32/ex-1-monday-quotes.py
+++ day-32/ex-1-monday-quotes.py
@@ -13,8 +13,6 @@ if weekday == 0:
         all_quotes = quote_file.readlines()
         quote = random.choice(all_quotes)
 
-    print(quote)
-
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(MY_EMAIL, MY_PASSWORD)
