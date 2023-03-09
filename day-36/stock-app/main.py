0fad7e367a326d455dc7f5b159b357e30f96c39f
Azad Asanali <azneotech@gmail.com>
2023-03-09 21:18:53 +0800

    Day 36: Stock News App - Step 2: Get the news article

--- day-36/stock-app/main.py
+++ day-36/stock-app/main.py
@@ -8,10 +8,11 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 STOCK_API_KEY = "FAKE_STOCK_API_KEY"
 NEWS_API_KEY = "FAKE_NEWS_API_KEY"
-## STEP 1: Use https://www.alphavantage.co/documentation/#daily
+# STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
-# Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
+# Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries.
+# e.g. [new_value for (key, value) in dictionary.items()]
 stock_params = {
     "function": "TIME_SERIES_DAILY_ADJUSTED",
     "symbol": STOCK_NAME,
@@ -30,17 +31,19 @@ day_before_yesterday_data = data_list[1]
 day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
 print(day_before_yesterday_closing_price)
 
-# Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
+# Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20.
+# Hint: https://www.w3schools.com/python/ref_func_abs.asp
 difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
 print(difference)
 
-# Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
+# Work out the percentage difference in price between closing price yesterday and closing price the
+# day before yesterday.
 diff_percent = (difference / float(yesterday_closing_price)) * 100
 print(diff_percent)
 
 # If percentage is greater than X then print("Get News").
 if diff_percent > 2:
-    ## STEP 2: https://newsapi.org/
+    # STEP 2: https://newsapi.org/
     # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
     news_params = {
         "apiKey": NEWS_API_KEY,
@@ -55,7 +58,7 @@ if diff_percent > 2:
     three_articles = articles[:3]
     print(three_articles)
 
-## STEP 3: Use twilio.com/docs/sms/quickstart/python
+# STEP 3: Use twilio.com/docs/sms/quickstart/python
 # to send a separate message with each article's title and description to your phone number.
 
 # TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
