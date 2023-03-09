dfce54702fc6fa472986708021ea37de52c7eee8
Azad Asanali <azneotech@gmail.com>
2023-03-09 21:28:06 +0800

    Day 36: Stock News App - Step 3: Final Refinements

--- day-36/stock-app/main.py
+++ day-36/stock-app/main.py
@@ -24,22 +24,18 @@ data = response.json()["Time Series (Daily)"]
 data_list = [value for (key, value) in data.items()]
 yesterday_data = data_list[0]
 yesterday_closing_price = yesterday_data["4. close"]
-print(yesterday_closing_price)
 
 # Get the day before yesterday's closing stock price
 day_before_yesterday_data = data_list[1]
 day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
-print(day_before_yesterday_closing_price)
 
 # Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20.
 # Hint: https://www.w3schools.com/python/ref_func_abs.asp
 difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
-print(difference)
 
 # Work out the percentage difference in price between closing price yesterday and closing price the
 # day before yesterday.
 diff_percent = (difference / float(yesterday_closing_price)) * 100
-print(diff_percent)
 
 # If percentage is greater than X then print("Get News").
 if diff_percent > 2:
@@ -56,12 +52,17 @@ if diff_percent > 2:
     # Use Python slice operator to create a list that contains the first 3 articles.
     # Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
     three_articles = articles[:3]
-    print(three_articles)
+
+    # Create a new list of the first 3 article's headline and description using list comprehension.
+    formatted_articles = [f"Headline: {article['title']}.\nBrief: {article['description']}" for article in three_articles]
+
+    for article in formatted_articles:
+        print(f"{article}\n")
 
 # STEP 3: Use twilio.com/docs/sms/quickstart/python
 # to send a separate message with each article's title and description to your phone number.
 
-# TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
+
 
 # TODO 9. - Send each article as a separate message via Twilio.
 
