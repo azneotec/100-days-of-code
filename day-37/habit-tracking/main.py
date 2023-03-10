9e29b3bd37a9166e8c9593f1072fcc9c64efe60c
Azad Asanali <azneotech@gmail.com>
2023-03-10 09:22:15 +0800

    Day 37: Project - Habit Tracker : PUT and DELETE requests

--- day-37/habit-tracking/main.py
+++ day-37/habit-tracking/main.py
@@ -29,8 +29,6 @@ graph_config = {
     "color": "ajisai",
 }
 
-
-
 # response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 # print(response)
 
@@ -39,11 +37,23 @@ pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
 today = datetime(year=2023, month=3, day=9)
 # today = datetime.now()
 # print(today.strftime("%Y%m%d"))
-
 pixel_data = {
     "date": today.strftime("%Y%m%d"),
     "quantity": "3.95"
 }
 
-response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+# response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+# print(response.text)
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+new_pixel_data = {
+    "quantity": "5.95"
+}
+
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+response = requests.delete(url=delete_endpoint, headers=headers)
 print(response.text)
