3a39602cb3fe94fe42484447fdeeb7c2d60c0ec2
Azad Asanali <azneotech@gmail.com>
2023-03-10 09:12:04 +0800

    Day 37: Project - Habit Tracker : Add pixel using post request

--- day-37/habit-tracking/main.py
+++ day-37/habit-tracking/main.py
@@ -2,9 +2,13 @@ import requests
 
 USERNAME = "fake_user"
 TOKEN = "fake_token"
+GRAPH_ID = "fake_graph_id"
+
+headers = {
+    "X-USER-TOKEN": TOKEN,
+}
 
 pixela_endpoint = "https://pixe.la/v1/users"
-
 user_params = {
     "token": TOKEN,
     "username": USERNAME,
@@ -16,18 +20,24 @@ user_params = {
 # print(response.text)
 
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
-
 graph_config = {
-    "id": "graph1",
+    "id": GRAPH_ID,
     "name": "Cycling Graph",
     "unit": "Km",
     "type": "float",
     "color": "ajisai",
 }
 
-headers = {
-    "X-USER-TOKEN": TOKEN,
+
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response)
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+pixel_data = {
+    "date": "20230310",
+    "quantity": "5.22"
 }
 
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
-print(response)
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+print(response.text)
