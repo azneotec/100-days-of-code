d95db31d6c50949db4dd662cae321d2e6652adde
Azad Asanali <azneotech@gmail.com>
2023-03-12 11:58:26 +0800

    Day 39-40: Capstone - Flight Deal Finder : Get the IATA Codes using the Kiwi Partners API

--- day-39-40/flight-deal-finder/flight_search.py
+++ day-39-40/flight-deal-finder/flight_search.py
@@ -1,8 +1,29 @@
+import requests
+
+
 class FlightSearch:
     # This class is responsible for talking to the Flight Search API.
-    def __init__(self, city_name):
-        self.city_name = city_name
+    def __init__(self, location_query_endpoint, api_key):
+        self.location_query_endpoint = location_query_endpoint
+        self.api_key = api_key
 
-    def search(self):
-        print(self.city_name)
-        return "TESTING"
+    def search(self, city_name):
+        search_data = {
+            "term": city_name,
+            "locale": "en-US",
+            "location_types": "airport",
+            "limit": "10",
+            "active_only": "true",
+        }
+        headers = {
+            "accept": "application/json",
+            "apikey": self.api_key,
+        }
+        response = requests.get(
+            url=self.location_query_endpoint,
+            params=search_data,
+            headers=headers
+        )
+        response.raise_for_status()
+        iata_code = response.json()["locations"][0]["city"]["code"]
+        return iata_code
--- day-39-40/flight-deal-finder/main.py
+++ day-39-40/flight-deal-finder/main.py
@@ -6,17 +6,34 @@ from flight_search import FlightSearch
 from data_manager import DataManager
 
 SHEETY_API_ENDPOINT = os.environ["SHEETY_API_ENDPOINT"]
+KIWI_LOCATION_SEARCH_API_ENDPOINT = os.environ["KIWI_LOCATION_SEARCH_API_ENDPOINT"]
+KIWI_API_KEY = os.environ["KIWI_API_KEY"]
+
 data_manager = DataManager(SHEETY_API_ENDPOINT)
+flight_search = FlightSearch(KIWI_LOCATION_SEARCH_API_ENDPOINT, KIWI_API_KEY)
 
 flight_deals_prices_response = requests.get(url=SHEETY_API_ENDPOINT)
 flight_deals_prices_response.raise_for_status()
 sheet_data = flight_deals_prices_response.json()["prices"]
+# sheet_data = [
+#     {
+#         "iataCode": "",
+#         "city": "Paris",
+#         "id": "2",
+#         "lowest_Price": "54",
+#     },
+#     {
+#         "iataCode": "",
+#         "city": "Berlin",
+#         "id": "3",
+#         "lowest_Price": "42",
+#     }
+# ]
 pprint(sheet_data)
 
 for sheet in sheet_data:
     if sheet["iataCode"] == "":
-        flight_search = FlightSearch(sheet["city"])
-        result = flight_search.search()
+        result = flight_search.search(sheet["city"])
         sheet["iataCode"] = result
 
         flight_deals_price_data = {
